492c7679c7ac80c216bba7cc626ec795b0d5e50b
ldx <kencani@qq.com>
2019-09-23 15:37:55 +0800

    创建项目

---
+++ openwtester/subscribe_test.go
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2018 The openwallet Authors
+ * This file is part of the openwallet library.
+ *
+ * The openwallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The openwallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package openwtester
+
+import (
+	"github.com/astaxie/beego/config"
+	"github.com/blocktree/openwallet/log"
+	"github.com/blocktree/openwallet/openw"
+	"github.com/blocktree/openwallet/openwallet"
+	"path/filepath"
+	"testing"
+)
+
+////////////////////////// 测试单个扫描器 //////////////////////////
+
+type subscriberSingle struct {
+}
+
+//BlockScanNotify 新区块扫描完成通知
+func (sub *subscriberSingle) BlockScanNotify(header *openwallet.BlockHeader) error {
+	log.Notice("header:", header)
+	return nil
+}
+
+//BlockTxExtractDataNotify 区块提取结果通知
+func (sub *subscriberSingle) BlockExtractDataNotify(sourceKey string, data *openwallet.TxExtractData) error {
+	log.Notice("account:", sourceKey)
+
+	for i, input := range data.TxInputs {
+		log.Std.Notice("data.TxInputs[%d]: %+v", i, input)
+	}
+
+	for i, output := range data.TxOutputs {
+		log.Std.Notice("data.TxOutputs[%d]: %+v", i, output)
+	}
+
+	log.Std.Notice("data.Transaction: %+v", data.Transaction)
+
+	return nil
+}
+
+func TestSubscribeAddress_ETH(t *testing.T) {
+
+	var (
+		endRunning = make(chan bool, 1)
+		symbol     = "BETH"
+		//accountID  = "HgRBsaiKgoVDagwezos496vqKQCh41pY44JbhW65YA8t"
+		addrs      = map[string]string{
+			"0x0f220906f237e8e56ad573cd7e258f672c6191d9": "receiver—1",
+			"0x95c28febbae551ea91c075cc6ef7ed4eaefad229": "receiver-2",
+			"0x774e0300f118feff15284090dd8c77aac75f3a2d": "receiver",
+			"0x1f44cf061cfd6420cb703c2ddae6df5c650bbce4": "receiver",
+			"0xbb2cc9a3d25dc5b7391e7be1d0b672e3f9d53de9": "receiver",
+			"0x31930f7146b11f068d6f2f28d20a9a43b91c44cf": "receiver",
+			"0x0a3aefdb58a03d7c30b2a063e1fd49c4cb5f8b60": "contract",
+			"0x98c862d9defa745f2761d09e2385e133679caa5f": "sender",
+			"0x5ba73d2a84d0b38c56a9caa05fe0f742d7182e5e": "sender",
+		}
+	)
+
+	//GetSourceKeyByAddress 获取地址对应的数据源标识
+	scanAddressFunc := func(address string) (string, bool) {
+		key, ok := addrs[address]
+		if !ok {
+			return "", false
+		}
+		return key, true
+	}
+
+	assetsMgr, err := openw.GetAssetsAdapter(symbol)
+	if err != nil {
+		log.Error(symbol, "is not support")
+		return
+	}
+
+	//读取配置
+	absFile := filepath.Join(configFilePath, symbol+".ini")
+
+	c, err := config.NewConfig("ini", absFile)
+	if err != nil {
+		return
+	}
+	assetsMgr.LoadAssetsConfig(c)
+
+	assetsLogger := assetsMgr.GetAssetsLogger()
+	if assetsLogger != nil {
+		assetsLogger.SetLogFuncCall(true)
+	}
+
+	//log.Debug("already got scanner:", assetsMgr)
+	scanner := assetsMgr.GetBlockScanner()
+	scanner.SetRescanBlockHeight(8275447)
+
+	if scanner == nil {
+		log.Error(symbol, "is not support block scan")
+		return
+	}
+
+	scanner.SetBlockScanAddressFunc(scanAddressFunc)
+
+	sub := subscriberSingle{}
+	scanner.AddObserver(&sub)
+
+	scanner.Run()
+
+	<-endRunning
+}
+
+func TestBlockScanner_ExtractTransactionData(t *testing.T) {
+
+	var (
+		symbol = "BETH"
+		txid   = "0xdeb8e107d0305b3a9134f489befb7dc7ec367384ef13ca041e139a760fd97a3c"
+		addrs  = map[string]string{
+			"0x7b7cec354d2d9f91e736e6042116f0fe2e3e332a": "sender",
+		}
+	)
+
+	//GetSourceKeyByAddress 获取地址对应的数据源标识
+	scanTargetFunc := func(target openwallet.ScanTarget) (string, bool) {
+		key, ok := addrs[target.Address]
+		if !ok {
+			return "", false
+		}
+		return key, true
+	}
+
+	assetsMgr, err := openw.GetAssetsAdapter(symbol)
+	if err != nil {
+		log.Error(symbol, "is not support")
+		return
+	}
+
+	//读取配置
+	absFile := filepath.Join(configFilePath, symbol+".ini")
+
+	c, err := config.NewConfig("ini", absFile)
+	if err != nil {
+		return
+	}
+	assetsMgr.LoadAssetsConfig(c)
+
+	assetsLogger := assetsMgr.GetAssetsLogger()
+	if assetsLogger != nil {
+		assetsLogger.SetLogFuncCall(true)
+	}
+
+	//log.Debug("already got scanner:", assetsMgr)
+	scanner := assetsMgr.GetBlockScanner()
+	//scanner.SetRescanBlockHeight(6518561)
+
+	if scanner == nil {
+		log.Error(symbol, "is not support block scan")
+		return
+	}
+	result, err := scanner.ExtractTransactionData(txid, scanTargetFunc)
+	if err != nil {
+		t.Errorf("ExtractTransactionData unexpected error %v", err)
+		return
+	}
+
+	for sourceKey, keyData := range result {
+		log.Notice("account:", sourceKey)
+		for _, data := range keyData {
+
+			for i, input := range data.TxInputs {
+				log.Std.Notice("data.TxInputs[%d]: %+v", i, input)
+			}
+
+			for i, output := range data.TxOutputs {
+				log.Std.Notice("data.TxOutputs[%d]: %+v", i, output)
+			}
+
+			log.Std.Notice("data.Transaction: %+v", data.Transaction)
+		}
+	}
+
+}
